cb7dfefd48dc936df385495883d0f468f59283a8
husilu <husilu@starlinke.com>
2022-11-10 20:01:40 +0800

    fearture: 添加用户信息弹窗，以及退出登陆弹窗

--- src/pages/Chatroom/ToolPanel.tsx
+++ src/pages/Chatroom/ToolPanel.tsx
@@ -1,12 +1,14 @@
-import { AiOutlineBulb, AiFillGithub, AiOutlineLogout } from "react-icons/ai";
+import { AiFillGithub, AiOutlineLogout } from "react-icons/ai";
+import { TbBrandWindows } from "react-icons/tb";
 import { RiTShirt2Line } from "react-icons/ri";
 import { UserInfo } from "src/store/user";
 import cx from "classnames";
 import styles from "./index.module.scss";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Modal from "src/components/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "../../store/theme";
+import {useNavigate} from 'react-router-dom'
 interface IProps {
   content?: string;
   user: UserInfo;
@@ -17,8 +19,9 @@ export default function ToolPanel(props: IProps) {
   let theme = useSelector((state: any) => state.theme.theme);
   const [Modaltext, setModaltext] = useState(<></>); // 弹窗内容
   const [ModalVisible, setModalVisible] = useState(false); // 是否显示弹窗
+  const navigate = useNavigate();
   let iconArray = [
-    { icon: <AiOutlineBulb></AiOutlineBulb>, title: "消息" },
+    { icon: <TbBrandWindows onClick={personModal}></TbBrandWindows>, title: "个人信息" },
     {
       icon: <RiTShirt2Line onClick={changeThemeModal}></RiTShirt2Line>,
       title: "换肤",
@@ -28,45 +31,98 @@ export default function ToolPanel(props: IProps) {
       title: "仓库链接",
       link: "https://github.com/husilu/chatroom-fe",
     },
-    { icon: <AiOutlineLogout></AiOutlineLogout>, title: "登出" },
+    { icon: <AiOutlineLogout onClick={logoutModal}></AiOutlineLogout>, title: "登出" },
   ];
   let themes = [
     { name: "粉色1", className: "theme-pink1" },
     { name: "粉色2", className: "theme-pink2" },
     { name: "粉色3", className: "theme-pink3" },
   ];
+
   useEffect(() => {
-    generator_color_modal();
+    if (ModalVisible) {
+      generator_color_modal();
+    }
   }, [theme]);
-  function generator_color_modal() {
-    setModaltext(
-      <div className="grid-cols-3">
-        {themes.map((item) => {
-          return (
-            <div
-              className={cx([
-                item.className,
-                styles.themesItem,
-                item.className === theme ? styles.active : "",
-              ])}
-              key={item.name}
-              onClick={() => changeColor(item.className)}
-            >
-              {item.name}
-            </div>
-          );
-        })}
-      </div>
-    );
+
+  // 构造颜色弹窗
+  const generator_color_modal = () => {
+    setTimeout(() => {
+      setModaltext(
+        <div className="grid-cols-3">
+          {themes.map((item) => {
+            return (
+              <div
+                className={cx([
+                  item.className,
+                  styles.themesItem,
+                  item.className === theme ? styles.active : "",
+                ])}
+                key={item.name}
+                onClick={() => changeColor(item.className)}
+              >
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
+      );
+    })
   }
+
+  // 构造用户弹窗
+  const generator_person_modal = () => {
+    setTimeout(() => {
+      setModaltext(
+        <div>
+          <img src="" alt="" />
+          <input type="upload" />
+          <p>昵称</p>
+          <input type="text" />
+          <button>提交</button>
+        </div>
+      );
+    })
+  }
+  // 构造登出弹窗
+  const generator_logout_modal = () => {
+    setTimeout(() => {
+      setModaltext(
+        <div>
+          <p>退出登陆？</p>
+          <button onClick={logoutHandler}>确认</button>
+          <button>取消</button>
+        </div>
+      );
+    })
+  }
+
   function changeThemeModal() {
     generator_color_modal();
     setModaltext(Modaltext);
     setModalVisible(true);
   }
+
+ function personModal() {
+    generator_person_modal();
+    setModaltext(Modaltext);
+    setModalVisible(true);
+  }
+
+  function logoutHandler() {
+    navigate('/login')
+  }
+
+  function logoutModal() {
+    generator_logout_modal()
+    setModaltext(Modaltext);
+    setModalVisible(true);
+  }
+
   function onClose() {
     setModalVisible(false);
   }
+
   function changeColor(color: string) {
     dispatch(changeTheme(color));
   }
